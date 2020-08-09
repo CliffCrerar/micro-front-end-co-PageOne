44aedb181054d39c9e7bb0745624871717707ff5
CliffCrerar <cliff.crerar@gmail.com>
2020-08-09 05:05:41 +0200

    minor

--- src/root.component.js
+++ src/root.component.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "./root.component.css";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  // return <section>{props.name} is mounted!</section>;
+  return (
+    <div className="container1">
+      <p>Page 1 App</p>
+    </div>
+  );
 }
